3df43cc90e53511d49e1fee378fc767e0b3bfbff
Omar Elbaga <oelbaga@newworldgroup.com>
2023-02-11 21:05:01 -0500

    add digital ocean ubuntu connection

--- src/lib/db.js
+++ src/lib/db.js
@@ -2,9 +2,18 @@ import mysql from "mysql2/promise";
 import fs from "fs";
 
 export async function query({ query, values = [] }) {
-  const dbconnection = await mysql.createConnection(
-    process.env.MYSQL_DATABASE_URL
-  );
+  // PlanetScale;
+  // const dbconnection = await mysql.createConnection(
+  //   process.env.MYSQL_DATABASE_URL
+  // );
+
+  //Digital ocean ubuntu
+  const dbconnection = await mysql.createConnection({
+    host: process.env.MYSQL_HOST,
+    database: process.env.MYSQL_DATABASE,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+  });
   try {
     const [results] = await dbconnection.execute(query, values);
     dbconnection.end();
