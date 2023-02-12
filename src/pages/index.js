25b3f2b77868fe03195305438dbf10054047b945
Omar Elbaga <oelbaga@newworldgroup.com>
2023-02-11 20:09:43 -0500

    update database url env

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import { useEffect, useState, useRef } from "react";
 import Head from "next/head";
 import Image from "next/image";
 import styles from "@/styles/Home.module.scss";
+
 export default function Home() {
   const productNameRef = useRef();
   const productIDToDeleteRef = useRef();
