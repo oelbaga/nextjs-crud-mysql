234d7e89f53937973d4ebb3672daa0a779bb824d
Omar Elbaga <oelbaga@newworldgroup.com>
2023-02-11 21:06:24 -0500

    switch to planetscale db

--- src/lib/db.js
+++ src/lib/db.js
@@ -3,17 +3,18 @@ import fs from "fs";
 
 export async function query({ query, values = [] }) {
   // PlanetScale;
-  // const dbconnection = await mysql.createConnection(
-  //   process.env.MYSQL_DATABASE_URL
-  // );
+  const dbconnection = await mysql.createConnection(
+    process.env.MYSQL_DATABASE_URL
+  );
 
   //Digital ocean ubuntu
-  const dbconnection = await mysql.createConnection({
-    host: process.env.MYSQL_HOST,
-    database: process.env.MYSQL_DATABASE,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-  });
+  // const dbconnection = await mysql.createConnection({
+  //   host: process.env.MYSQL_HOST,
+  //   database: process.env.MYSQL_DATABASE,
+  //   user: process.env.MYSQL_USER,
+  //   password: process.env.MYSQL_PASSWORD,
+  // });
+
   try {
     const [results] = await dbconnection.execute(query, values);
     dbconnection.end();
